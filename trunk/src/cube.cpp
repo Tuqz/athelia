7202d73efa461efcbdf1e2f3ad142f436d0ddca0
tuqz <tuqz@06583610-a295-4c4a-b5fc-e8ce88ae7c3b>
2014-02-17 19:39:26 +0000

    Converted to non-OGRE code

--- trunk/src/cube.cpp
+++ trunk/src/cube.cpp
@@ -1,189 +1,36 @@
 #include "cube.h"
 
-cube::cube() {
+Cube::Cube() {
+	pos.x = 0;
+	pos.y = 0;
+	pos.z = 0;
+	size = 0;
+	gen_arrays();
 }
 
-cube::cube(Ogre::SceneManager* SceneMgr) {
-  plus_xy = Ogre::Plane(Ogre::Vector3::UNIT_Z, 0.5);
-  plus_xz = Ogre::Plane(Ogre::Vector3::UNIT_Y, 0.5);
-  plus_yz = Ogre::Plane(Ogre::Vector3::UNIT_X, 0.5);
-
-  minus_xy = Ogre::Plane(-Ogre::Vector3::UNIT_Z, 0.5);
-  minus_xz = Ogre::Plane(-Ogre::Vector3::UNIT_Y, 0.5);
-  minus_yz = Ogre::Plane(-Ogre::Vector3::UNIT_X, 0.5);
-
-  Ogre::MeshManager::getSingleton().createPlane("plus_xy", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_xy, 1, 1, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_X);
-  Ogre::MeshManager::getSingleton().createPlane("plus_xz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_xz, 1, 1, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_Z);
-  Ogre::MeshManager::getSingleton().createPlane("plus_yz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_yz, 1, 1, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_Y);
-  Ogre::MeshManager::getSingleton().createPlane("minus_xy", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_xy, 1, 1, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_X);
-  Ogre::MeshManager::getSingleton().createPlane("minus_xz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_xz, 1, 1, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_Z);
-  Ogre::MeshManager::getSingleton().createPlane("minus_yz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_yz, 1, 1, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_Y);
-
-  Ogre::Entity* ent_plus_xy = SceneMgr->createEntity("plus_xy_ent", "plus_xy");
-  Ogre::Entity* ent_plus_xz = SceneMgr->createEntity("plus_xz_ent", "plus_xz");
-  Ogre::Entity* ent_plus_yz = SceneMgr->createEntity("plus_yz_ent", "plus_yz");
-  Ogre::Entity* ent_minus_xy = SceneMgr->createEntity("minus_xy_ent", "minus_xy");
-  Ogre::Entity* ent_minus_xz = SceneMgr->createEntity("minus_xz_ent", "minus_xz");
-  Ogre::Entity* ent_minus_yz = SceneMgr->createEntity("minus_yz_ent", "minus_yz");
-
-
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_plus_xy);
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_plus_xz);
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_plus_yz);
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_minus_xy);
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_minus_xz);
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_minus_yz);
-
-  ent_plus_xy->setCastShadows(true);
-  ent_plus_xz->setCastShadows(true);
-  ent_plus_yz->setCastShadows(true);
-  ent_minus_xy->setCastShadows(true);
-  ent_minus_xz->setCastShadows(true);
-  ent_minus_yz->setCastShadows(true);
+Cube::Cube(double cntrx, double cntry, double cntrz, double scale) {
+	pos.x = cntrx;
+	pos.y = cntry;
+	pos.z = cntrz;
+	size = scale;
+	gen_arrays();
 }
 
-cube::cube(double scale, Ogre::SceneManager* SceneMgr) {
-  plus_xy = Ogre::Plane(Ogre::Vector3::UNIT_Z, scale/2);
-  plus_xz = Ogre::Plane(Ogre::Vector3::UNIT_Y, scale/2);
-  plus_yz = Ogre::Plane(Ogre::Vector3::UNIT_X, scale/2);
-
-  minus_xy = Ogre::Plane(-Ogre::Vector3::UNIT_Z, scale/2);
-  minus_xz = Ogre::Plane(-Ogre::Vector3::UNIT_Y, scale/2);
-  minus_yz = Ogre::Plane(-Ogre::Vector3::UNIT_X, scale/2);
-
-  Ogre::MeshManager::getSingleton().createPlane("plus_xy", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_xy, scale, scale, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_X);
-  Ogre::MeshManager::getSingleton().createPlane("plus_xz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_xz, scale, scale, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_Z);
-  Ogre::MeshManager::getSingleton().createPlane("plus_yz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_yz, scale, scale, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_Y);
-  Ogre::MeshManager::getSingleton().createPlane("minus_xy", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_xy, scale, scale, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_X);
-  Ogre::MeshManager::getSingleton().createPlane("minus_xz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_xz, scale, scale, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_Z);
-  Ogre::MeshManager::getSingleton().createPlane("minus_yz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_yz, scale, scale, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_Y);
-
-  Ogre::Entity* ent_plus_xy = SceneMgr->createEntity("plus_xy_ent", "plus_xy");
-  Ogre::Entity* ent_plus_xz = SceneMgr->createEntity("plus_xz_ent", "plus_xz");
-  Ogre::Entity* ent_plus_yz = SceneMgr->createEntity("plus_yz_ent", "plus_yz");
-  Ogre::Entity* ent_minus_xy = SceneMgr->createEntity("minus_xy_ent", "minus_xy");
-  Ogre::Entity* ent_minus_xz = SceneMgr->createEntity("minus_xz_ent", "minus_xz");
-  Ogre::Entity* ent_minus_yz = SceneMgr->createEntity("minus_yz_ent", "minus_yz");
-
-
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_plus_xy);
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_plus_xz);
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_plus_yz);
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_minus_xy);
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_minus_xz);
-  SceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent_minus_yz);
-
-  ent_plus_xy->setCastShadows(true);
-  ent_plus_xz->setCastShadows(true);
-  ent_plus_yz->setCastShadows(true);
-  ent_minus_xy->setCastShadows(true);
-  ent_minus_xz->setCastShadows(true);
-  ent_minus_yz->setCastShadows(true);
+Cube::Cube(Vector place, double scale) {
+	pos = place;
+	size = scale;
+	gen_arrays();
 }
 
-cube::cube(double x_centre, double y_centre, double z_centre, Ogre::SceneManager* SceneMgr) {
-  plus_xy = Ogre::Plane(Ogre::Vector3::UNIT_Z, 0.5);
-  plus_xz = Ogre::Plane(Ogre::Vector3::UNIT_Y, 0.5);
-  plus_yz = Ogre::Plane(Ogre::Vector3::UNIT_X, 0.5);
+const int Cube::faces[6][4] = {{0, 1, 2, 4}, {0, 1, 3, 5}, {1, 5, 4, 7}, {0, 3, 2, 6}, {3, 5, 6, 7}, {2, 4, 6, 7}};
 
-  minus_xy = Ogre::Plane(-Ogre::Vector3::UNIT_Z, 0.5);
-  minus_xz = Ogre::Plane(-Ogre::Vector3::UNIT_Y, 0.5);
-  minus_yz = Ogre::Plane(-Ogre::Vector3::UNIT_X, 0.5);
-
-  Ogre::MeshManager::getSingleton().createPlane("plus_xy", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_xy, 1, 1, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_X);
-  Ogre::MeshManager::getSingleton().createPlane("plus_xz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_xz, 1, 1, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_Z);
-  Ogre::MeshManager::getSingleton().createPlane("plus_yz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_yz, 1, 1, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_Y);
-  Ogre::MeshManager::getSingleton().createPlane("minus_xy", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_xy, 1, 1, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_X);
-  Ogre::MeshManager::getSingleton().createPlane("minus_xz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_xz, 1, 1, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_Z);
-  Ogre::MeshManager::getSingleton().createPlane("minus_yz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_yz, 1, 1, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_Y);
-
-  Ogre::Entity* ent_plus_xy = SceneMgr->createEntity("plus_xy_ent", "plus_xy");
-  Ogre::Entity* ent_plus_xz = SceneMgr->createEntity("plus_xz_ent", "plus_xz");
-  Ogre::Entity* ent_plus_yz = SceneMgr->createEntity("plus_yz_ent", "plus_yz");
-  Ogre::Entity* ent_minus_xy = SceneMgr->createEntity("minus_xy_ent", "minus_xy");
-  Ogre::Entity* ent_minus_xz = SceneMgr->createEntity("minus_xz_ent", "minus_xz");
-  Ogre::Entity* ent_minus_yz = SceneMgr->createEntity("minus_yz_ent", "minus_yz");
-
-
-  Ogre::SceneNode* xy_plus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  xy_plus->attachObject(ent_plus_xy);
-  Ogre::SceneNode* xz_plus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  xz_plus->attachObject(ent_plus_xz);
-  Ogre::SceneNode* yz_plus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  yz_plus->attachObject(ent_plus_yz);
-  Ogre::SceneNode* xy_minus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  xy_minus->attachObject(ent_minus_xy);
-  Ogre::SceneNode* xz_minus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  xz_minus->attachObject(ent_minus_xz);
-  Ogre::SceneNode* yz_minus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  yz_minus->attachObject(ent_minus_yz);
-
-  xy_plus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-  xz_plus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-  yz_plus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-  xy_minus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-  xz_minus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-  yz_minus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-
-  ent_plus_xy->setCastShadows(true);
-  ent_plus_xz->setCastShadows(true);
-  ent_plus_yz->setCastShadows(true);
-  ent_minus_xy->setCastShadows(true);
-  ent_minus_xz->setCastShadows(true);
-  ent_minus_yz->setCastShadows(true);
-}
-
-cube::cube(double x_centre, double y_centre, double z_centre, double scale, Ogre::SceneManager* SceneMgr) {
-  plus_xy = Ogre::Plane(Ogre::Vector3::UNIT_Z, scale/2);
-  plus_xz = Ogre::Plane(Ogre::Vector3::UNIT_Y, scale/2);
-  plus_yz = Ogre::Plane(Ogre::Vector3::UNIT_X, scale/2);
-
-  minus_xy = Ogre::Plane(-Ogre::Vector3::UNIT_Z, scale/2);
-  minus_xz = Ogre::Plane(-Ogre::Vector3::UNIT_Y, scale/2);
-  minus_yz = Ogre::Plane(-Ogre::Vector3::UNIT_X, scale/2);
-
-  Ogre::MeshManager::getSingleton().createPlane("plus_xy", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_xy, scale, scale, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_X);
-  Ogre::MeshManager::getSingleton().createPlane("plus_xz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_xz, scale, scale, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_Z);
-  Ogre::MeshManager::getSingleton().createPlane("plus_yz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plus_yz, scale, scale, 20, 20, true, 1, 1, 1, Ogre::Vector3::UNIT_Y);
-  Ogre::MeshManager::getSingleton().createPlane("minus_xy", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_xy, scale, scale, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_X);
-  Ogre::MeshManager::getSingleton().createPlane("minus_xz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_xz, scale, scale, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_Z);
-  Ogre::MeshManager::getSingleton().createPlane("minus_yz", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, minus_yz, scale, scale, 20, 20, true, 1, 1, 1, -Ogre::Vector3::UNIT_Y);
-
-  Ogre::Entity* ent_plus_xy = SceneMgr->createEntity("plus_xy_ent", "plus_xy");
-  Ogre::Entity* ent_plus_xz = SceneMgr->createEntity("plus_xz_ent", "plus_xz");
-  Ogre::Entity* ent_plus_yz = SceneMgr->createEntity("plus_yz_ent", "plus_yz");
-  Ogre::Entity* ent_minus_xy = SceneMgr->createEntity("minus_xy_ent", "minus_xy");
-  Ogre::Entity* ent_minus_xz = SceneMgr->createEntity("minus_xz_ent", "minus_xz");
-  Ogre::Entity* ent_minus_yz = SceneMgr->createEntity("minus_yz_ent", "minus_yz");
-
-
-  Ogre::SceneNode* xy_plus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  xy_plus->attachObject(ent_plus_xy);
-  Ogre::SceneNode* xz_plus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  xz_plus->attachObject(ent_plus_xz);
-  Ogre::SceneNode* yz_plus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  yz_plus->attachObject(ent_plus_yz);
-  Ogre::SceneNode* xy_minus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  xy_minus->attachObject(ent_minus_xy);
-  Ogre::SceneNode* xz_minus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  xz_minus->attachObject(ent_minus_xz);
-  Ogre::SceneNode* yz_minus = SceneMgr->getRootSceneNode()->createChildSceneNode();
-  yz_minus->attachObject(ent_minus_yz);
-
-  xy_plus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-  xz_plus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-  yz_plus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-  xy_minus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-  xz_minus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-  yz_minus->translate(Ogre::Vector3(x_centre, y_centre, z_centre));
-
-  ent_plus_xy->setCastShadows(true);
-  ent_plus_xz->setCastShadows(true);
-  ent_plus_yz->setCastShadows(true);
-  ent_minus_xy->setCastShadows(true);
-  ent_minus_xz->setCastShadows(true);
-  ent_minus_yz->setCastShadows(true);
-}
-
-cube::~cube() {
+void Cube::gen_arrays() {
+	verts[0] = {pos.x + (size / 2), pos.y + (size / 2), pos.z + (size / 2)};
+	verts[1] = {pos.x - (size / 2), pos.y + (size / 2), pos.z + (size / 2)};
+	verts[2] = {pos.x + (size / 2), pos.y - (size / 2), pos.z + (size / 2)};
+	verts[3] = {pos.x + (size / 2), pos.y + (size / 2), pos.z - (size / 2)};
+	verts[4] = {pos.x - (size / 2), pos.y - (size / 2), pos.z + (size / 2)};
+	verts[5] = {pos.x - (size / 2), pos.y + (size / 2), pos.z - (size / 2)};
+	verts[6] = {pos.x + (size / 2), pos.y - (size / 2), pos.z - (size / 2)};
+	verts[7] = {pos.x - (size / 2), pos.y - (size / 2), pos.z - (size / 2)};
 }
--- trunk/src/cube.h
+++ trunk/src/cube.h
@@ -1,22 +1,16 @@
-#include "OGRE/OgrePlane.h"
-#include "OGRE/OgreMeshManager.h"
-#include "OGRE/OgreEntity.h"
-#include "OGRE/OgreSceneManager.h"
+#pragma once
 
-class cube {
- public:
-  cube();
-  cube(Ogre::SceneManager* SceneMgr);
-  cube(double scale, Ogre::SceneManager* SceneMgr);
-  cube(double x_centre, double y_centre, double z_centre, Ogre::SceneManager* SceneMgr);
-  cube(double x_centre, double y_centre, double z_centre, double scale, Ogre::SceneManager* SceneMgr);
-  void texture(Ogre::SceneManager* SceneMgr, Ogre::String path_to_texture);
-  ~cube();
- private:
-  Ogre::Plane plus_xy;
-  Ogre::Plane plus_xz;
-  Ogre::Plane plus_yz;
-  Ogre::Plane minus_xy;
-  Ogre::Plane minus_xz;
-  Ogre::Plane minus_yz;
+#include "vector.h"
+
+class Cube {
+	public:
+	Vector pos;
+	double size;
+	Cube();
+	Cube(double cntrx, double cntry, double cntrz, double scale);
+	Cube(Vector place, double scale);
+	private:
+	void gen_arrays();
+	Vector verts[8];
+	static const int faces[6][4]; 
 };
