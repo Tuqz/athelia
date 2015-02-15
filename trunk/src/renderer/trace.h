94c1491898bd5269ec4f018e8f4c323bcb5f0c3e
tuqz <tuqz@06583610-a295-4c4a-b5fc-e8ce88ae7c3b>
2015-02-15 20:26:54 +0000

    Clean slate for new OGRE rendering and scons building.

--- trunk/src/renderer/trace.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "ray.h"
-#include "colour.h"
-#include "renderable.h"
-namespace Renderer {
-	Colour trace(Ray ray, std::vector<Renderable*> entities);
-}
--- trunk/techdemos/lua/run_file/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <lua.h>
-
-int main() {
-	Lua::LuaInterface interface;
-	interface.run_file("hello.lua");
-	return 0;
-}
